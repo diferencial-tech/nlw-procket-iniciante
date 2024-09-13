8c4758daa09fb931afede15fcc126efb965ceb48
Messias Neto <messias@diferencialtech.com.br>
2024-09-13 15:34:30 -0300

    add json de metas

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 let mensagem = 'Bem vindo ao app de metas!'
 
-let meta = {
-  value: 'Tomar 3L de água por dia',
-  checked: false,
+let metas
+
+const carregarMetas = async () => {
+  try {
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas,null,2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input( {message: "Digite a meta:"})
@@ -54,6 +64,10 @@ const listarMeta = async () => {
 }
 
 const metasRealizadas = async () => {
+  if(metas.length == 0 ) {
+    mensagem = "Não existe nenhuma meta!"
+    return
+  }
   const realizadas = metas.filter((meta) => {
     return meta.checked
   })
@@ -71,6 +85,10 @@ const metasRealizadas = async () => {
 }
 
 const metasAbertas = async () => {
+  if(metas.length == 0 ) {
+    mensagem = "Não existe nenhuma meta!"
+    return
+  }
   const abertas = metas.filter((meta) => {
     return meta.checked != true
   })
@@ -88,6 +106,10 @@ const metasAbertas = async () => {
 }
 
 const deletarMetas = async () => {
+  if(metas.length == 0 ) {
+    mensagem = "Não existe nenhuma meta!"
+    return
+  }
   const metasDesmarcadas = metas.map((meta) => {
     return {value: meta.value, checked: false}
   })
@@ -122,9 +144,12 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
+  await carregarMetas()
 
   while(true){
     mostrarMensagem()
+    await salvarMetas()
+
     const opcao = await select({
       message: "Menu >",
       choices: [
